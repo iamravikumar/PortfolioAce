d75a160644b065161cc0957808fbc90929314a53
RamonWill <ramonwilliams@hotmail.co.uk>
2020-11-02 12:20:01 +0000

    creating initial views and viewmodels

---
+++ PortfolioAce/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PortfolioAce.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioAce.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        public INavigator Navigator { get; set; } = new Navigator();
+    }
+}
