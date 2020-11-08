4468bb3a62fc0c12f1640e4d273e8535c9bfbbbe
RamonWill <ramonwilliams@hotmail.co.uk>
2020-11-08 14:14:57 +0000

    implementing listbox command to update view

---
+++ PortfolioAce/Commands/SelectFundCommand.cs
@@ -0,0 +1,28 @@
+﻿using PortfolioAce.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PortfolioAce.Commands
+{
+    public class SelectFundCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private AllFundsViewModel _allFundsWindowVM;
+        public SelectFundCommand(AllFundsViewModel allFundsViewModel)
+        {
+            allFundsViewModel = _allFundsWindowVM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true; //For now
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine(parameter);
+        }
+    }
+}
--- PortfolioAce/ViewModels/AllFundsViewModel.cs
+++ PortfolioAce/ViewModels/AllFundsViewModel.cs
@@ -1,9 +1,11 @@
-﻿using PortfolioAce.Domain.Models;
+﻿using PortfolioAce.Commands;
+using PortfolioAce.Domain.Models;
 using PortfolioAce.EFCore.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace PortfolioAce.ViewModels
 {
@@ -15,7 +17,11 @@ namespace PortfolioAce.ViewModels
             //
             _fundRepo = fundRepo;
             _lbFunds = fundRepo.GetAllFunds().ToList();
+            _currentFund = (_lbFunds.Count!=0) ? _lbFunds[0] : null;
+            SelectFundCommand = new SelectFundCommand(this);
         }
+
+        public ICommand SelectFundCommand { get; set; }
 
         // List box click should have a command and that command changes the fields displayed on the right of the allfundsview.
         private List<Fund> _lbFunds;
@@ -31,5 +37,20 @@ namespace PortfolioAce.ViewModels
                 OnPropertyChanged(nameof(lbFunds));
             }
         }
+
+        private Fund _currentFund;
+        public string CurrentFund
+        {
+            get
+            {
+                return _currentFund.FundName;
+            }
+            set
+            {
+                _currentFund = _lbFunds.Where(f => f.FundName==value).FirstOrDefault();
+                OnPropertyChanged(nameof(CurrentFund));
+            }
+        }
+
     }
 }
