2c69e9d546de369681c4390b5d5701035dbe8674
RamonWill <ramonwilliams@hotmail.co.uk>
2020-11-27 00:14:47 +0000

    notes

--- PortfolioAce.Domain/Models/FactTables/PositionFACT.cs
+++ PortfolioAce.Domain/Models/FactTables/PositionFACT.cs
@@ -10,6 +10,8 @@ namespace PortfolioAce.Domain.Models.FactTables
     [Table("fact_Positions")]
     public class PositionFACT
     {
+        // I need to a pseudo "position ID" unique for this position. so that i can link it to Trades... this will also allow me to calculate open lots..."
+        // Trades will have a foreign key linking to this position
         [Key]
         public int PositionId { get; set; }
         [Required]
@@ -19,13 +21,13 @@ namespace PortfolioAce.Domain.Models.FactTables
         [Required]
         public decimal AverageCost { get; set; }
         [Required]
-        public decimal MarketValue { get; set; }
+        public decimal MarketValue { get; set; } // this cannot be static. use computed column https://docs.microsoft.com/en-us/ef/core/what-is-new/ef-core-5.0/whatsnew
         [Required]
         public decimal RealisedPnl { get; set; }
         [Required]
-        public decimal UnrealisedPnl { get; set; }
+        public decimal UnrealisedPnl { get; set; } // this cannot be static. use computed column https://docs.microsoft.com/en-us/ef/core/what-is-new/ef-core-5.0/whatsnew
         [Required]
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } // this cannot be static. reference the security have to reference
 
         [ForeignKey("Fund")]
         public int FundId { get; set; }
