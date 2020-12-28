24e446ea54ad0f6ff7df0002a4a47ef1b172c565
RamonWill <ramonwilliams@hotmail.co.uk>
2020-12-28 19:32:14 +0000

    created new fact table service for metrics

---
+++ PortfolioAce.EFCore/Services/FactTableServices/FactTableService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioAce.EFCore.Services.FactTableServices
+{
+    public class FactTableService:IFactTableService
+    {
+        private readonly PortfolioAceDbContextFactory _contextFactory;
+
+
+        public FactTableService(PortfolioAceDbContextFactory contextFactory)
+        {
+            this._contextFactory = contextFactory;
+        }
+    }
+}
