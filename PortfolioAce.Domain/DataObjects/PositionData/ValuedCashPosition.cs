5906c67bab384f628fe61759b89bb531585b765e
RamonWill <ramonwilliams@hotmail.co.uk>
2021-01-18 16:33:26 +0000

    fix cash asset valuations

--- PortfolioAce.Domain/DataObjects/PositionData/ValuedCashPosition.cs
+++ PortfolioAce.Domain/DataObjects/PositionData/ValuedCashPosition.cs
@@ -6,42 +6,41 @@ namespace PortfolioAce.Domain.DataObjects.PositionData
 {
     public abstract class ValuedCashPosition:ValuedPosition
     {
-        public abstract CalculatedCashPosition CashPosition { get; set; }
-        public abstract decimal MarketValueLocal { get; set; }
-        public abstract decimal fxRate { get; set; }
-        public abstract decimal MarketValueBase { get; set; }
+        public abstract CalculatedCashPosition CashPosition { get; }
+        public abstract decimal MarketValueLocal { get;  }
+        public abstract decimal fxRate { get;  }
+        public abstract decimal MarketValueBase { get; }
         public abstract bool IsValuedBase { get; set; } // This determines whether fxRates and Market prices are available on this date...
         public abstract DateTime AsOfDate { get; set; }
     }
     
     public class ValuedLiquidCashPosition : ValuedCashPosition
     {
-        public override CalculatedCashPosition CashPosition { get; set; }
-        public override decimal MarketValueLocal { get; set; }
-        public override decimal fxRate { get; set; }
-        public override decimal MarketValueBase { get; set; }
+        public override CalculatedCashPosition CashPosition { get; }
+        public override decimal MarketValueLocal { get { return CashPosition.NetQuantity; } }
+        public override decimal fxRate { get { return _fxRate; } }
+        public override decimal MarketValueBase { get { return MarketValueLocal / fxRate; } }
         public override bool IsValuedBase { get; set; } // This determines whether fxRates and Market prices are available on this date...
         public override DateTime AsOfDate { get; set; }
-
+        private decimal _fxRate;
         public ValuedLiquidCashPosition(CalculatedCashPosition cashPosition, Dictionary<(string, DateTime), decimal> priceTable, DateTime asOfDate, string FundBaseCurrency)
         {
-
-            this.AsOfDate = asOfDate;
-            // this is for cash holdings I can get the valuation in base currency
             this.CashPosition = cashPosition;
-            if (cashPosition.Currency.Symbol == FundBaseCurrency)
+            this.AsOfDate = asOfDate;
+
+            string localCurrency = cashPosition.Currency.Symbol;
+            if (localCurrency == FundBaseCurrency)
             {
                 IsValuedBase = true;
-                this.fxRate = decimal.One;
+                _fxRate = decimal.One;
             }
             else
             {
-                string fxSymbol = $"{cashPosition.Currency}{FundBaseCurrency}";
+                string fxSymbol = $"{localCurrency}{FundBaseCurrency}";
                 ValueTuple<string, DateTime> tableKeyFx = (fxSymbol, asOfDate);
                 this.IsValuedBase = priceTable.ContainsKey(tableKeyFx);
-                this.fxRate = (priceTable.ContainsKey(tableKeyFx)) ? priceTable[tableKeyFx] : decimal.One;
+                _fxRate = (this.IsValuedBase) ? priceTable[tableKeyFx] : decimal.One;
             }
-            this.MarketValueBase = Math.Round(this.CashPosition.NetQuantity * fxRate, 2);
         }
     }
 
