6ca595031f696640d31c9972b69d465da0b0cb2a
RamonWill <ramonwilliams@hotmail.co.uk>
2021-02-16 22:54:30 +0000

    create the basis for security suggestions

---
+++ PortfolioAce/Models/Providers/SecuritySuggestionProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoCompleteTextBox.Editors;
+using System.Linq;
+using PortfolioAce.Domain.Models.Dimensions;
+using System.Collections;
+
+namespace PortfolioAce.Models.Providers
+{
+    public class SecuritySuggestionProvider:ISuggestionProvider
+    {
+        public IEnumerable<SecuritiesDIM> SecurityList { get; set; }
+
+        public SecuritiesDIM GetExactSuggestion(string filter)
+        {
+            if (string.IsNullOrWhiteSpace(filter)) return null;
+            return
+                SecurityList
+                    .FirstOrDefault(security => string.Equals(security.Symbol, filter, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        public IEnumerable<SecuritiesDIM> GetSuggestions(string filter)
+        {
+            if (string.IsNullOrWhiteSpace(filter)) return null;
+            System.Threading.Thread.Sleep(1000);
+            return
+                SecurityList
+                    .Where(security => security.Symbol.IndexOf(filter, StringComparison.CurrentCultureIgnoreCase) > -1)
+                    .ToList();
+        }
+
+
+
+        IEnumerable GetFullCollection()
+        {
+            return SecurityList.ToList();
+        }
+
+        IEnumerable ISuggestionProvider.GetSuggestions(string filter)
+        {
+            return GetSuggestions(filter);
+        }
+
+        public SecuritySuggestionProvider()
+        {
+            IEnumerable<SecuritiesDIM> securities = new List<SecuritiesDIM> { 
+                                                    new SecuritiesDIM { SecurityName="Advanced Micro Devices", Symbol="AMD"},
+                                                    new SecuritiesDIM { SecurityName="Apple", Symbol="AAPL"}
+                                                    }; // i need a way to pass in the list of securities....
+            SecurityList = securities;
+        }
+    }
+}
