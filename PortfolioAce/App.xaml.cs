23f28710f1ade2d86cd1d7867612cb9b58beb7a2
RamonWill <ramonwilliams@hotmail.co.uk>
2021-02-16 01:55:21 +0000

    update copyright year

--- PortfolioAce/App.xaml.cs
+++ PortfolioAce/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2020 Ramon Williams
+﻿// Copyright © 2021 Ramon Williams
 // GNU General Public License
 // This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; 
 // either version 3 of the License, or (at your option) any later version.
